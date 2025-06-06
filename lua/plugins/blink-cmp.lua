8ff9d32638ef36479de4282778ad3bbc7608e5aa
Dmitry Lyubimcev <dzone@danwin1210.de>
2025-06-06 15:39:40 +0300

    fix: add pcall to blink-cmp's documentation window
    Signed-off-by: Dmitry Lyubimcev <dzone@danwin1210.de>

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -45,10 +45,12 @@ return {
                 auto_show_delay_ms = 0,
 
                 draw = function(opts)
-				    if opts.item and opts.item.documentation then
-					    local out = require("pretty_hover.parser").parse(opts.item.documentation.value)
-					    opts.item.documentation.value = out:string()
-				    end
+                    pcall(function()
+				        if opts.item and opts.item.documentation then
+					        local out = require("pretty_hover.parser").parse(opts.item.documentation.value)
+					        opts.item.documentation.value = out:string()
+				        end
+                    end)
 
 				    opts.default_implementation(opts)
 			    end,
