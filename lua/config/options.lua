fc8c0b165b185c9b6e516d5865021ad555c11d91
Dmitry Lyubimcev <dzone@danwin1210.de>
2026-08-03 21:35:31 +0300

    feat: set colorcolumn to 85
    Signed-off-by: Dmitry Lyubimcev <dzone@danwin1210.de>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -35,6 +35,7 @@ function module.init()
     vim.opt.tabstop = 4
     vim.opt.softtabstop = 4
     vim.opt.shiftwidth = 4
+    vim.opt.colorcolumn = "85"
     vim.opt.breakindent = true
     vim.opt.smartindent = true
     vim.opt.expandtab = true
